db693bcb549585b89022d014fe9bdc30cef50a3e
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-04-26 22:28:01 +0900

    [追加] ポイントカードマスタの一覧・新規作成

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const User = require("./models/User.model.js");
 const Card = require("./models/Card.model.js");
+const CardMaster = require("./models/CardMaster.model.js");
 const router = express.Router();
 
 router.get('/registered', (req, res) => {
@@ -23,5 +24,40 @@ router.get('/registered', (req, res) => {
     res.json(rescards);
   })();
 });
+
+/*
+ * ポイントカードマスタ 
+ */
+
+// 自分が管理しているポイントカードマスタ一覧取得
+router.get('/master', (req, res) => {
+  (async function () {
+    const currentUser = await User.findOne({
+      where: {
+        AccessToken: 'TESTACTKN1'
+      }
+    });
+    const masters = await CardMaster.findAll({
+      where: {
+        OwnerUser: currentUser.id
+      }
+    });
+    const resmasters = masters.map(m => ({
+      id: m.ID,
+      style: m.Style
+    }));
+    res.json(resmasters);
+  })();
+});
+
+// ポイントカードマスタの新規作成
+router.post('/add-master', (req, res) => {
+  console.log(req.body);
+  const newMaster = CardMaster.create({
+    'Style': req.body.style,
+    'OwnerUser': 1 // DUMMY!
+  });
+  res.json({result: 'ok'});
+})
 
 module.exports = router;
