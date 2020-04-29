d42cf422d65024b590e42f1760ec1fb8198f8a0e
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-04-29 22:53:40 +0900

    [修正] テスト用DBセットアップのスクリプトのパスを修正

--- test/util.js
+++ test/util.js
@@ -2,9 +2,9 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 module.exports.updb = function () {
-  execSync(path.resolve('../UpTestDB.sh'));
+  execSync(path.resolve('./UpTestDB.sh'));
 }
 
 module.exports.downdb = function () {
-  execSync(path.resolve('../DownTestDB.sh'));
+  execSync(path.resolve('./DownTestDB.sh'));
 }
