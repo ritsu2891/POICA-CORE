ae9f5dd77ffe93740a71ef31b62ec8f547029883
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-04-30 15:14:59 +0900

    [変更] テスト環境の調整

--- config/db.js
+++ config/db.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require('sequelize');
+var express = require("express");
+var app = express();
 
-module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PSWD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql'
-});
+if (app.get('env') != 'test') {
+  module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PSWD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  });
+} else {
+  module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PSWD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+  });
+}
