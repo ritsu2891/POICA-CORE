8eab0862381f6a5435645a32adbab989986220bb
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-04-30 21:07:37 +0900

    [追加] ポイント付与権限尾確認

--- app/controllers/validators.js
+++ app/controllers/validators.js
@@ -1,17 +1,44 @@
 const { isEmpty } = require('../util.js');
 
-module.exports.denyEmptyResult = function(target, name) {
+const models = require('../models');
+const Card = models.Card;
+
+function denyEmptyResult(target, name) {
   if (isEmpty(target)) {
     throw new Error(name + '_NOT_FOUND');
   }
   return;
 }
+module.exports.denyEmptyResult = denyEmptyResult;
 
 module.exports.cardCanRegisterByUser = function(master) {
   if (!master.canRegisterByUser()) {
     throw new Error('CANNOT_REGISTER_BY_USER');
   }
   return;
+}
+
+module.exports.checkPointGiveAuthority = async function(master, userid, as) {
+  if (as == 'admin') {
+    if (userid != master.ownerUserId) {
+      throw new Error('NO_PERMISSION');
+    }
+  } else if (as == 'user') {
+    if (!master.userToUserPointOpt) {
+      throw new Error('NO_PERMISSION');
+    }
+    const operatorCard = await Card.findOne({
+      where: {
+        masterId: master.id,
+        ownerUserId: userid,
+      }
+    });
+    try {
+      denyEmptyResult(operatorCard, '');
+    } catch (e) {
+      throw new Error('NO_PERMISSION');
+    }
+  }
 }
 
 module.exports.denyInvalidRegToken = function(master, token) {
@@ -21,4 +48,12 @@ module.exports.denyInvalidRegToken = function(master, token) {
     }
   }
   return;
+}
+
+module.exports.denyNonNumerical = function(value) {
+  const numValue = Number(value);
+  if (!Number.isInteger(numValue)) {
+    throw new Error('INVALID_VALUE');
+    return;
+  }
 }
