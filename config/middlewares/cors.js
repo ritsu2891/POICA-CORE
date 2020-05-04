bee8f8e1139602ce5ff90bb27313dacaac334cd9
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-05-04 14:34:23 +0900

    [追加] CORSでGETとPOSTのみ許可するよう変更

--- config/middlewares/cors.js
+++ config/middlewares/cors.js
@@ -1,5 +1,6 @@
 module.exports = function (req, res, next) {
   res.set('Access-Control-Allow-Origin', process.env.FE_URL);
   res.set('Access-Control-Allow-Headers', 'X-POICA-Access-Token');
+  res.set('Access-Control-Request-Method', 'GET, POST');
   next();
 };
