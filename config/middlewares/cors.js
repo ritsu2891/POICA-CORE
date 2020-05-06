0be5ce525b14de537bfb9dee15a3b32d4c80bb17
Ritsuki KOKUBO <ritsu2891@gmail.com>
2020-05-06 22:01:37 +0900

    [変更] CORSの許可ヘッダにContent-Typeを追加 JSONのPOSTを受け入れるため

--- config/middlewares/cors.js
+++ config/middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
   res.set('Access-Control-Allow-Origin', `http://${process.env.WEB_APP_FQDN}`);
-  res.set('Access-Control-Allow-Headers', 'X-POICA-Access-Token');
+  res.set('Access-Control-Allow-Headers', 'X-POICA-Access-Token, Content-Type');
   res.set('Access-Control-Request-Method', 'GET, POST');
   next();
 };
