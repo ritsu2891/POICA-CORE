98d6aded73f98be7c278f3d14274ffc4fe8a07d6
Ritsuki KOKUBO <rpaka@outlook.jp>
2021-05-06 17:08:58 +0900

    Setup.shの環境を選べるようにした

--- Setup.sh
+++ Setup.sh
@@ -1,4 +1,4 @@
-eval "$(cat config/.env.development <(echo) <(declare -x))"
+eval "$(cat config/.env.$1 <(echo) <(declare -x))"
 mkdir -p $UPLOAD_PATH
 mkdir -p $UPLOAD_PATH/master
 mkdir -p $UPLOAD_PATH/master/cardimage
