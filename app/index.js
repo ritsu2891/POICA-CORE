ed7119dbea0d0caba384babb01f365ce958e78c8
Ritsuki KOKUBO <rpaka@outlook.jp>
2021-05-06 17:17:07 +0900

    ポート番号をenvファイルで指定

--- app/index.js
+++ app/index.js
@@ -65,7 +65,7 @@ if (app.get('env') != 'test') {
     res.json({});
   });
 
-  var server = app.listen(4000, function () {
+  var server = app.listen(process.env.PORT || 4000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
